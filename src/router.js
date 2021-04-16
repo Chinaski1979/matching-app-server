be977c54eba13af29ebce5f4c435cd5f47d8e84a
Jorge <cascante.george@gmail.com>
2021-04-16 17:27:52 -0600

    Basic initial files

---
+++ src/router.js
@@ -0,0 +1,8 @@
+// import { Router } from 'express';
+// // import formResponseController from './controllers/formsResponses/formResponsesController';
+
+// const router = Router();
+
+// router.get('/example', () => console.log('yooo'));
+const router = 'foo';
+export default router;
