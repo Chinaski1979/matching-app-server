1788a690ea90b4e5a93aec4061bf39d939d65014
Jorge <cascante.george@gmail.com>
2021-04-16 17:27:52 -0600

    Basic initial files

---
+++ index.js
@@ -0,0 +1,20 @@
+// const express = require('express');
+import express from 'express';
+import cors from'cors';
+import router from './src/router.js';
+console.log('router', router);
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+// app.use('/api/', router);
+
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Node API listening on port', port);
+});
---
+++ src/controllers/formsResponses/formResponsesController.js
@@ -0,0 +1,9 @@
+
+function getForm(req, res) {
+  console.log('6666');
+  res.send('about birds');
+}
+
+module.exports = {
+  getForm,
+};
